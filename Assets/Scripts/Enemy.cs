eb9d37243737f10409b1ed18da7570bbf11f089e
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2016-08-09 20:03:04 +0900

    init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	GameObject target;
+	public GameObject shot;
+	public GameObject enemyMuzzle;
+
+	float shotInterval = 0;
+	float shotIntervalMax = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		// ターゲットを取得
+		target = GameObject.Find("PlayerTarget");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// ターゲットの方向を向く
+		transform.LookAt(target.transform);
+
+		shotInterval += Time.deltaTime;
+
+		if (shotInterval > shotIntervalMax) {
+
+			Debug.Log("self: " + transform.position);
+			Debug.Log ("target: " + target.transform.position);
+
+			Instantiate (shot, enemyMuzzle.transform.position, transform.rotation);
+			shotInterval = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/ShotPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotPlayer : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+		// 出現後一定時間で自動的に消滅させる
+		Destroy(gameObject, 2.0F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 弾を前進させる
+		transform.position += transform.forward * Time.deltaTime * 100;
+	}
+
+	// 他のオブジェクトと衝突した時に呼ばれるメソッド
+	private void OnCollisionEnter(Collision collider) {
+		if (collider.gameObject.name == "Terrain") {
+			Destroy (gameObject);
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
+	}
+}
