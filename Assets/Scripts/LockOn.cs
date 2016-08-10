2099178167b0b4d700b861c108de51a2c97fdd35
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2016-08-10 11:24:10 +0900

    add lockOn

---
+++ Assets/Scripts/LockOn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockOn : MonoBehaviour {
+
+	GameObject target = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Lock")) {
+			if (target != null) {
+				target = null;
+			} else {
+				// ターゲットを取得する
+				target = GameObject.FindWithTag("Enemy");
+			}
+		}
+
+		if (target != null) {
+			// ターゲットの方向を向く
+			transform.LookAt(target.transform);
+			transform.rotation = new Quaternion (0, transform.rotation.y, 0, transform.rotation.w);
+		}
+	}
+}
