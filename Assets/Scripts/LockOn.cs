4ee562308108d2a52c7d236fd9e074af3f47487e
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2016-08-10 11:46:29 +0900

    smooth lockOn

--- Assets/Scripts/LockOn.cs
+++ Assets/Scripts/LockOn.cs
@@ -24,6 +24,14 @@ public class LockOn : MonoBehaviour {
 		if (target != null) {
 			// ターゲットの方向を向く
 			transform.LookAt(target.transform);
+
+			// スムーズにターゲットの方向を向く
+			// Quaternion.LookRotation(A - B); B位置からA位置を向いた状態の向きを算出
+			Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+			// Quaternion.Slerp(現在の向き, 目標の向き, 歩合); 自身がターゲットに徐々に向くように、3つ目の引数（歩合）＝回転のスピード。
+			transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, Time.deltaTime * 10);
+
+			// LookAtの際、XやZ軸もtargetの方向を向いてしまうのを補正
 			transform.rotation = new Quaternion (0, transform.rotation.y, 0, transform.rotation.w);
 		}
 	}
